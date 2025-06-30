1709ac8e6d8343521bf5694ffae38250283c1711
Saar Wasserman <saarwasserman@Saars-MacBook.local>
2025-06-30 14:50:07 +0300

    initial local commit

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+from httpx import ASGITransport, AsyncClient
+from app.main import app
+
+import pytest
+
+
+@pytest.mark.anyio
+async def test_root():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        response = await ac.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello Python"}
